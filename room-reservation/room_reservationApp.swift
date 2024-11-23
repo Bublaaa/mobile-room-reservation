6b51926485e6b90d10224b1eeae60dd1f6a0ab4e
Bublaaa <yanuarkrisna7@gmail.com>
2024-11-23 15:00:47 +0700

    Initial Commit

---
+++ room-reservation/room_reservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  room_reservationApp.swift
+//  room-reservation
+//
+//  Created by Krisna Yanuardi on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct room_reservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ room-reservationTests/room_reservationTests.swift
@@ -0,0 +1,17 @@
+//
+//  room_reservationTests.swift
+//  room-reservationTests
+//
+//  Created by Krisna Yanuardi on 23/11/24.
+//
+
+import Testing
+@testable import room_reservation
+
+struct room_reservationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
